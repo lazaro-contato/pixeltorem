f1db473a147cc6f6b3685d3b1507dbc55eca41c2
José Lázaro <contato.jlazaro@gmail.com>
2021-05-23 16:07:17 -0300

    adding logic

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ var root = 16;
 var pixel = 0;
 var rem = 0;
 
+
+// Return values //
+
+var valuepixel = $("#input-px").val()
+
+
+$("#button-copy-1").click(function() {
+    console.log(valuepixel)
+})
+
 // Calculate //
 
 var rem_calculate = (pixel / root);
 var pixel_calculate = (root / rem);
+
+
+// Adding changes //
+
+$("#input-px").on('input', function(){
+    $("#input-rem").val("25")
+})
+
