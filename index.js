b9ef09c8d562b6a570b545c11ab20373ce015e98
José Lázaro <contato.jlazaro@gmail.com>
2021-05-24 17:15:29 -0300

    adding the math

--- index.js
+++ index.js
@@ -42,11 +42,21 @@ $("#input-rem").keyup(function(){
 })
 
 
+// ========== COPY WITH BUTTON ========== //
 
+$("#button-copy-1").click(function(){
+    $("#input-px").select()
+    document.execCommand('copy');
+})
 
+$("#button-copy-2").click(function(){
+    $("#input-rem").select()
+    document.execCommand('copy');
+})
+
+/*
 var rem_calculate = (pixel / root);
 var pixel_calculate = (root / rem);
-
-
+*/
 
 
