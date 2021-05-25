51128ec9ebd195a147288d82055b6920c0e1ac1a
José Lázaro <contato.jlazaro@gmail.com>
2021-05-24 22:12:34 -0300

    finishing the logic

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+/* Math Mathodes
+var rem_calculate = (pixel / root);
+var pixel_calculate = (root / rem);
+*/
+
 // Variables //
 
 var root = 16;
@@ -5,25 +11,15 @@ var pixel = 0;
 var rem = 0;
 
 
-// ========== CHANGING THE ROOT ========== //
-
-$("#input-root").keyup(function(){
-    root = Number($(this).val());
-
-    var valueRem = Number($("#input-rem").val());
-    $("#input-px").val(root * valueRem);
-})
-
-
 
 // ========== CHANGING INPUT REM ========== //
 
 $("#input-px").keyup(function(){
     var valuepixel = Number($(this).val());
-    $("#input-rem").val(valuepixel / root);
+    $("#input-rem").val((valuepixel / root).toFixed(3));
 
     if (!$(this).val()){
-        $("#input-rem").val("")
+        $("#input-rem").val("");
     }
 
     
@@ -33,30 +29,47 @@ $("#input-px").keyup(function(){
 
 $("#input-rem").keyup(function(){
     var valueRem = Number($(this).val());
-    $("#input-px").val(root / valueRem);
+    $("#input-px").val((root / valueRem).toFixed(2));
 
     if (!$(this).val()){
-        $("#input-px").val("")
+        $("#input-px").val("");
     }
+
+    var valueRem = Number($("#input-rem").val());
+    $("#input-px").val((root * valueRem).toFixed(2));
+
+    if(!valueRem){
+        $("#input-px").val("");
+    };
 
 })
 
+// ========== CHANGING THE ROOT ========== //
+
+$("#input-root").keyup(function(){
+        root = Number($(this).val());
+        var valueRem = Number($("#input-rem").val());
+        $("#input-px").val((root * valueRem).toFixed(2));
+
+        if(!root){
+            $("#input-px").val("");
+        };
+
+})
 
 // ========== COPY WITH BUTTON ========== //
 
 $("#button-copy-1").click(function(){
-    $("#input-px").select()
+    $("#input-px").select();
     document.execCommand('copy');
 })
 
 $("#button-copy-2").click(function(){
-    $("#input-rem").select()
+    $("#input-rem").select();
     document.execCommand('copy');
 })
 
-/*
-var rem_calculate = (pixel / root);
-var pixel_calculate = (root / rem);
-*/
+
+
 
 
